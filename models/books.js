dedcb3d01c1fb38f4b5665dc2610f9346882f6e8
Una <una.lestrange@atu.ie>
2022-10-01 16:30:26 +0100

    can't figure out why validation isn't working

--- models/books.js
+++ models/books.js
@@ -1,9 +1,20 @@
 const mongoose  = require('mongoose');
 
-const bookSchema = new mongoose.Schema({
-    title: String,
-    year_written: Number
+const authorSchema = new mongoose.Schema({
+    name: {type: String, required : true},
+    contact: String,
+    age: {type: Number, max : 4}
 })
+
+const bookSchema = new mongoose.Schema({
+    play: {type: Number, required : true},
+    title: String ,
+    year_written: Number,
+    author: [authorSchema]
+})
+
+bookSchema.set('validateBeforeSave', true);
+authorSchema.set('validateBeforeSave', true);
 
 const Book = mongoose.model('book', bookSchema);
 
