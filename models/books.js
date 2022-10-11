c487f65bfe8f4830fa6d568885de6b6c50ffb67e
Una <una.lestrange@atu.ie>
2022-10-11 16:54:21 +0100

    ws6 done

--- models/books.js
+++ models/books.js
@@ -1,4 +1,5 @@
 const mongoose  = require('mongoose');
+const Joi = require('joi');
 
 const authorSchema = new mongoose.Schema({
     name: {type: String, required : true},
@@ -7,14 +8,29 @@ const authorSchema = new mongoose.Schema({
 
 const bookSchema = new mongoose.Schema({
     title: String,
-    author: String ,
     year_written: Number,
     author: authorSchema,
     edition: String
 })
 
+function ValidateBook(book) {
+
+   const authorJoiSchema = Joi.object({
+       name: Joi.string().min(2).required(),
+       nationality: Joi.string()
+   })
+
+    const bookJoiSchema = Joi.object({
+       title: Joi.string().min(3).required(),
+       author: authorJoiSchema,
+       year_written: Joi.number().integer().min(1600) ,
+       edition: Joi.string()
+    })
+    return bookJoiSchema.validate(book);
+}
+
 
 const Book = mongoose.model('book', bookSchema);
 
-module.exports = {Book}
+module.exports = {Book, ValidateBook}
 
