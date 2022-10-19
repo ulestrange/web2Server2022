93fdffece58b40d67d3e2d93840bcaa7959dfc74
Una <una.lestrange@atu.ie>
2022-10-19 12:15:48 +0100

    finished

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
-
+require('dotenv').config();
+// note this required a .env file which is not in github
 
 // own  modules
 const db = require('./database');
@@ -8,7 +9,7 @@ const books = require('./routes/books')
 const home = require('./routes/home')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3001
 
 
 
