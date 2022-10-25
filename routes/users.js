2c5f6854f34a5f1ee7cf18ebc95ddd96da4ff097
Una <una.lestrange@atu.ie>
2022-10-25 17:18:22 +0100

    fds

--- routes/users.js
+++ routes/users.js
@@ -9,21 +9,25 @@ const router = express.Router();
 
 
 router.post('/', async (req, res) => {
-    console.table(req.body);
+  console.table(req.body);
 
-    try {
-        let user = await User.register(
-          new User({ email:req.body.email, username:req.body.username }),
-            req.body.password);
+  try {
+    let user = await User.register(
+      new User({
+        email: req.body.email,
+        username: req.body.username,
+        roles: req.body.roles
+      }),
+      req.body.password);
 
-            res.location(user._id).
-            status(201).
-            json(user._id);
-    }
-    catch (error) {
-            res.status(400).json({error : error});
-            return;
-    }
+    res.location(user._id).
+      status(201).
+      json(user._id);
+  }
+  catch (error) {
+    res.status(400).json({ error: error });
+    return;
+  }
 })
 
 module.exports = router;
