3fca6390f4c27072d488197f7258499c5966c1ef
Una <una.lestrange@atu.ie>
2022-10-26 10:02:43 +0100

    some very simple logging added to illustrate middleware

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const {logging1, logging2} = require('../middleware/logging')
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Hello World from Una!'))
+router.use(logging1);
+
+router.get('/',  (req, res) => res.send('Hello World from Una!'))
+
+router.use(logging2)
 
 router.get('/bananas', (req, res) =>
   res.send('hello world, this is bananas'));
 
 
-  module.exports = router;
+ module.exports = router;
